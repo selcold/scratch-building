a30f7d2e375ad377c2e418357330ea6357b17d49
Fun117 <fun117.kun@gmail.com>
2024-03-30 19:33:16 +0900

    Update and fix.

--- scratch-auth.config.ts
+++ scratch-auth.config.ts
@@ -2,7 +2,7 @@ import { ScratchAuth_config } from "scratch-auth-react"
 
 // セットアップファイル内で必要な設定を行います
 const _scratchauth_config: ScratchAuth_config = {
-    redirect_url: `https://scratch-building.vercel.app/api/auth`, //リダイレクトURLを適切なものに変更してください
+    redirect_url: `https://scratch-building.vercel.app/api/auth/`, //リダイレクトURLを適切なものに変更してください
 }
 
 export default _scratchauth_config
