a713f67aafc6f14bc036270b5759cfa1ed50445c
Fun117 <fun117.kun@gmail.com>
2024-04-03 15:09:57 +0900

    Fixing Comment Functionality and Implementing New Features

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,15 @@ import { _cfgMaintenancePages } from '@/components/configs/config'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     // Check if the path is in the maintenance pages list
-    return NextResponse.redirect(new URL(`https://scratch-building.vercel.app/`, request.url))
+    if (_cfgMaintenancePages.includes(request.nextUrl.pathname)) {
+        return NextResponse.redirect(new URL(`/maintenance?requestUrl=${request.nextUrl.href}`, request.url))
+    } else {
+        if (request.nextUrl.pathname === "/play") {
+            return NextResponse.redirect(new URL(`/games`, request.url))
+        } else {
+            return NextResponse.next()
+        }
+    }
 }
 
 export const config = {
