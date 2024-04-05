383af4f4fe0a08ca5dcbd0a61beb1ad57e31db1a
Fun117 <fun117.kun@gmail.com>
2024-04-05 13:29:59 +0900

    Refactor CommentForm component to use ref for accessing textarea value

--- scratch-auth.config.ts
+++ scratch-auth.config.ts
@@ -2,10 +2,12 @@ import { ScratchAuth_config } from "scratch-auth-react/src/dist/config"
 
 // セットアップファイル内で必要な設定を行います
 const config: ScratchAuth_config = {
-    redirect_url: `http://localhost:3000/api/auth/`,
+    redirect_url: `https://scratch-building.vercel.app/api/auth/`,
+    // redirect_url: `http://localhost:3000/api/auth/`,
     title: `Scratch Building`,
 }
 
 export default config
 
 // https://scratch-building.vercel.app/api/auth/
+// http://localhost:3000/api/auth/
